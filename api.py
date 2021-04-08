eef523ee2dd4a25a11dc76312a2538ce655545cb
Arturo Filastò <arturo@filasto.net>
2021-04-08 18:42:15 +0200

    PoC using worktree

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 import os
 import io
 import csv
+import shutil
 
 from pprint import pprint
 
@@ -8,7 +9,7 @@ import git
 from gitdb import IStream
 from git.index.typ import BaseIndexEntry
 
-WORKING_DIR = "working_dir"
+WORKING_DIR = os.path.abspath("working_dir")
 REPO_DIR = os.path.join(WORKING_DIR, "test-lists")
 REPO_URL = "git@github.com:citizenlab/test-lists.git"
 
@@ -26,95 +27,70 @@ def init_repo():
     repo.remote().fetch(progress=ProgressPrinter())
     return repo
 
-def blob_to_file(blob):
-    tl_bytes = io.BytesIO()
-    blob.stream_data(tl_bytes)
-    test_list_content = io.TextIOWrapper(tl_bytes, encoding="utf-8")
-    test_list_content.seek(0)
-    return test_list_content
-
 class TestListManager:
     def __init__(self):
         self.repo = init_repo()
 
-    def get_tree(self, username):
-        head = self.repo.heads.master
-        # If there is a head with the username it means the user has made some
-        # changes, so we need to present the test-list from their branch
-        if username in self.repo.heads:
-            head = self.repo.heads[username]
-        return head.commit.tree
+    def get_user_repo(self, username):
+        repo_path = os.path.join(WORKING_DIR, "users", username)
+        if not os.path.exists(repo_path):
+            print(f"creating {repo_path}")
+            self.repo.git.worktree("add", repo_path)
+        return git.Repo(repo_path)
 
     def get_test_list(self, username):
+        repo_path = os.path.join(WORKING_DIR, "users", username)
+        if not os.path.exists(repo_path):
+            repo_path = REPO_DIR
+
         test_lists = {}
-        tree = self.get_tree(username)
-        lists_dir = tree.join("lists")
-        for fn in lists_dir.blobs:
-            cc = os.path.basename(fn.path).split(".")[0]
+        for path in glob(os.path.join("lists", "*.csv")):
+            cc = os.path.basename(path).split(".")[0]
             if not len(cc) == 2 and not cc == "global":
                 continue
-            tl_file = blob_to_file(fn)
-            csv_reader = csv.DictReader(tl_file)
-            for line in csv_reader:
-                test_lists[cc] = test_lists.get(cc, [])
-                test_lists[cc].append(dict(line))
+            with open(path) as tl_file:
+                csv_reader = csv.DictReader(tl_file)
+                for line in csv_reader:
+                    test_lists[cc] = test_lists.get(cc, [])
+                    test_lists[cc].append(dict(line))
         return test_lists
 
-    def write_commit(self, username, parent_commit, tl_string, filename, comment):
-        tl_string.seek(0)
-        tl_b = tl_string.read().encode("utf-8")
-        tl_bytes_len = len(tl_b)
-        tl_bytes = io.BytesIO(tl_b)
-
-        new_blob = self.repo.odb.store(IStream("blob", tl_bytes_len, tl_bytes))
-        entry = BaseIndexEntry((0o100644, new_blob.binsha, 0, filename))
-
-        index = git.IndexFile.from_tree(self.repo, parent_commit)
-        index.add([entry], write=False)
-        new_tree = index.write_tree()
-        new_commit = git.Commit.create_from_tree(self.repo, new_tree, comment, head=False)
-
-        self.repo.heads[username].commit = new_commit
-
     def add(self, username, cc, new_entry, comment):
-        if username not in self.repo.heads:
-            self.repo.create_head(username)
+        repo = self.get_user_repo(username)
 
-        parent_commit = self.repo.heads[username].commit
-        filename = f"lists/{cc}.csv"
+        filepath = os.path.join(WORKING_DIR, "users", username, "lists", f"{cc}.csv")
 
-        tl = parent_commit.tree.join(filename)
-        tl_string = io.StringIO()
-        csv_reader = csv.reader(blob_to_file(tl))
-        csv_writer = csv.writer(tl_string, quoting=csv.QUOTE_MINIMAL, lineterminator="\n")
-        for row in csv_reader:
-            csv_writer.writerow(row)
-        csv_writer.writerow(new_entry)
-
-        self.write_commit(username, parent_commit, tl_string, filename, comment)
+        with open(filepath, "a") as out_file:
+            csv_writer = csv.writer(out_file, quoting=csv.QUOTE_MINIMAL, lineterminator="\n")
+            csv_writer.writerow(new_entry)
+        repo.index.add([filepath])
+        repo.index.commit(comment)
 
     def edit(self, username, cc, old_entry, new_entry, comment):
-        if username not in self.repo.heads:
-            self.repo.create_head(username)
+        repo = self.get_user_repo(username)
 
-        parent_commit = self.repo.heads[username].commit
-        filename = f"lists/{cc}.csv"
+        filepath = os.path.join(WORKING_DIR, "users", username, "lists", f"{cc}.csv")
 
-        tl = parent_commit.tree.join(filename)
-        tl_string = io.StringIO()
-        csv_reader = csv.reader(blob_to_file(tl))
-        csv_writer = csv.writer(tl_string, quoting=csv.QUOTE_MINIMAL, lineterminator="\n")
-        found = False
-        for row in csv_reader:
-            if row == old_entry:
-                found = True
-                csv_writer.writerow(new_entry)
-            else:
-                csv_writer.writerow(row)
+        out_buffer = io.StringIO()
+        with open(filepath, "r") as in_file:
+            csv_reader = csv.reader(in_file)
+            csv_writer = csv.writer(out_buffer, quoting=csv.QUOTE_MINIMAL, lineterminator="\n")
+
+            found = False
+            for row in csv_reader:
+                if row == old_entry:
+                    found = True
+                    csv_writer.writerow(new_entry)
+                else:
+                    csv_writer.writerow(row)
         if not found:
             raise Exception("Could not find the specified row")
 
-        self.write_commit(username, parent_commit, tl_string, filename, comment)
+        with open(filepath, "w") as out_file:
+            out_buffer.seek(0)
+            shutil.copyfileobj(out_buffer, out_file)
+        repo.index.add([filepath])
+        repo.index.commit(comment)
 
 def main():
     tlm = TestListManager()
