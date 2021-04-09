e66c799e45c3f020973eef34fa864ec8acb7207d
Arturo Filastò <arturo@filasto.net>
2021-04-09 12:30:41 +0200

    Start fleshing out the state handling

--- api.py
+++ api.py
@@ -11,35 +11,83 @@ from git.index.typ import BaseIndexEntry
 
 WORKING_DIR = os.path.abspath("working_dir")
 REPO_DIR = os.path.join(WORKING_DIR, "test-lists")
-REPO_URL = "git@github.com:citizenlab/test-lists.git"
+MASTER_REPO_URL = "git@github.com:citizenlab/test-lists.git"
+PUSH_REPO_URL = "git@github.com:hellais/test-lists.git"
 
 class ProgressPrinter(git.RemoteProgress):
     def update(self, op_code, cur_count, max_count=None, message=""):
         print(op_code, cur_count, max_count, cur_count / (max_count or 100.0), message or "NO MESSAGE")
 
 def clone_repo():
-    git.Repo.clone_from(REPO_URL, REPO_DIR, branch="master")
+    return git.Repo.clone_from(MASTER_REPO_URL, REPO_DIR, branch="master")
 
 def init_repo():
     if not os.path.exists(REPO_DIR):
-        clone_repo()
+        repo = clone_repo()
+        repo.create_remote("rworigin", PUSH_REPO_URL)
     repo = git.Repo(REPO_DIR)
-    repo.remote().fetch(progress=ProgressPrinter())
+    repo.remotes.origin.pull(progress=ProgressPrinter())
     return repo
 
 class TestListManager:
     def __init__(self):
         self.repo = init_repo()
 
+    def get_user_repo_path(self, username):
+        return os.path.join(WORKING_DIR, "users", username, "test-lists")
+
+    def get_user_statefile_path(self, username):
+        return os.path.join(WORKING_DIR, "users", username, "state")
+
+    def get_user_pr_path(self, username):
+        return os.path.join(WORKING_DIR, "users", username, "pr_id")
+
+    def get_user_branchname(self, username):
+        return f"user-contribution/{username}"
+
+    def get_state(self, username):
+        """
+        Returns the current state of the repo for the given user.
+
+        The possible states are:
+        - CLEAN:
+            when we are in sync with the current tip of master and no changes have been made
+        - DIRTY:
+            when there are some changes in the working tree of the user, but they haven't yet pushed them
+        - PUSHING:
+            when we are pushing the changes made by the user via propose_changes
+        - PR_OPEN:
+            when the PR of the user is open on github and it's waiting for being merged
+        """
+        try:
+            with open(self.get_user_statefile_path(username), "r") as in_file:
+                return out_file.read()
+        except FileNotFoundError:
+            return "CLEAN"
+
+    def set_state(self, username, state):
+        """
+        This will record the current state of the pull request for the user to the statefile.
+
+        The absence of a statefile is an indication of a clean state.
+        """
+        assert state in ("DIRTY", "PUSHING", "PR_OPEN")
+        with open(self.get_user_statefile_path(username), "w") as out_file:
+            out_file.write(state)
+
+    def set_pr_id(self, username, pr_id):
+        with open(self.get_user_pr_path(username), "w") as out_file:
+            out_file.write(pr_id)
+
     def get_user_repo(self, username):
-        repo_path = os.path.join(WORKING_DIR, "users", username)
+        repo_path = self.get_user_repo_path(username)
         if not os.path.exists(repo_path):
             print(f"creating {repo_path}")
-            self.repo.git.worktree("add", repo_path)
+            self.repo.git.worktree("add", "-b", self.get_user_branchname(username), repo_path)
         return git.Repo(repo_path)
 
     def get_test_list(self, username):
-        repo_path = os.path.join(WORKING_DIR, "users", username)
+        repo_path = self.get_user_repo_path(username)
         if not os.path.exists(repo_path):
             repo_path = REPO_DIR
 
@@ -57,8 +105,10 @@ class TestListManager:
 
     def add(self, username, cc, new_entry, comment):
         repo = self.get_user_repo(username)
+        if state in ("PUSHING", "PR_OPEN"):
+            raise Exception("You cannot edit files while changes are pending")
 
-        filepath = os.path.join(WORKING_DIR, "users", username, "lists", f"{cc}.csv")
+        filepath = os.path.join(self.get_user_repo_path(username), "lists", f"{cc}.csv")
 
         with open(filepath, "a") as out_file:
             csv_writer = csv.writer(out_file, quoting=csv.QUOTE_MINIMAL, lineterminator="\n")
@@ -66,10 +116,16 @@ class TestListManager:
         repo.index.add([filepath])
         repo.index.commit(comment)
 
+        self.set_state(username, "DIRTY")
+
     def edit(self, username, cc, old_entry, new_entry, comment):
+        state = self.get_state(username)
+        if state in ("PUSHING", "PR_OPEN"):
+            raise Exception("You cannot edit the files while changes are pending")
+
         repo = self.get_user_repo(username)
 
-        filepath = os.path.join(WORKING_DIR, "users", username, "lists", f"{cc}.csv")
+        filepath = os.path.join(self.get_user_repo_path(username), "lists", f"{cc}.csv")
 
         out_buffer = io.StringIO()
         with open(filepath, "r") as in_file:
@@ -91,6 +147,25 @@ class TestListManager:
             shutil.copyfileobj(out_buffer, out_file)
         repo.index.add([filepath])
         repo.index.commit(comment)
+
+        self.set_state(username, "DIRTY")
+
+    def open_pr(self, branchname):
+        return "12345"
+
+    def is_pr_resolved(self, username):
+        return False
+
+    def propose_changes(self, username):
+        self.set_state(username, "PUSHING")
+
+        shutil.rmtree(self.get_user_repo_path(username))
+        self.repo.git.worktree("prune")
+        self.repo.remotes.rworigin.push(self.get_user_branchname(username), progress=ProgressPrinter(), force=True)
+
+        pr_id = self.open_pr(self.get_user_branchname(username))
+        self.set_pr_id(username, pr_id)
+        self.set_state(username, "PR_OPEN")
 
 def main():
     tlm = TestListManager()
@@ -119,6 +194,7 @@ def main():
         "",
         "Site reported to be blocked by AGCOM - Italian Autority on Communication"
     ], "add https to the website url")
+    tlm.propose_changes("antani")
 
 if __name__ == "__main__":
     main()
